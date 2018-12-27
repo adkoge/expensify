5aa675c6d1a9baa8b5fa721eb54e0bb842c577c9
Alan Koger <adkoge@gmail.com>
2018-12-27 11:22:15 -0500

    Worked with array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 // const person = {
 //   name: "Alan",
 //   age: 31,
@@ -16,16 +20,28 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: "Ego is the Enemy",
-  author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin"
-  }
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin"
+//   }
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
+// const { name: publisherName = "Self-Published" } = book.publisher;
 
-console.log(publisherName); // Penguin, Self-Published if no name
+// console.log(publisherName); // Penguin, Self-Published if no name
+
+//
+// Array destructuring
+//
+
+// const address = ["1299 S Juniper Street", "Philadelphia", "Pennsylvania", "19147"];
+// const [, city, state = "New York"] = address;
+// console.log(`You are in ${city} ${state}.`)
 
 
+const item = ["Coffee (hot)", "$2.00", "$2.50", "$2.75"];
+const [itemName, , medium] = item;
+
+console.log(`A medium ${itemName} costs ${medium}`);
