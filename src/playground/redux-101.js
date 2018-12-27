b1ccf0a3d99dc92ee5590defc8cd8d3c50fc2ffc
Alan Koger <adkoge@gmail.com>
2018-12-27 11:54:03 -0500

    Setup action generators

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,36 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
+
+// Action generators
+const incrementCount = ({ incrementBy = 1 } = {}) => ({ 
+  type: "INCREMENT",
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: "DECREMENT",
+  decrementBy
+});
+
+// setCount
+const setCount = ({ count } = {}) => ({
+  type: "SET",
+  count
+});
+
+// resetCount
+const resetCount = () => ({
+  type: "RESET"
+});
 
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1; 
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       };
     case "DECREMENT":
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy
+        count: state.count - action.decrementBy
       };
     case "SET":
       return {
@@ -29,31 +49,14 @@ const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
 
-// I'd like to increment the count
-store.dispatch({
-  type: "INCREMENT",
-  incrementBy: 5
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-  type: "INCREMENT"
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-  type: "RESET"
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 10
-});
+store.dispatch(decrementCount());
 
-store.dispatch({
-  type: "DECREMENT",
-  decrementBy: 1
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(setCount({ count: 101 }));
