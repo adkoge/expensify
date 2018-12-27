af868bad539b04ec3a5a1304d2d2dfa84ed45f0e
Alan Koger <adkoge@gmail.com>
2018-12-27 12:17:22 -0500

    Worked with combinereducerst to combine reducers and set up default state

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,49 @@
 import { createStore, combineReducers } from "redux";
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+const expensesReducerDefaultSate = [];
+
+const expensesReducer = (state = expensesReducerDefaultSate, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Filters Reducer
+const filtersReducerDefaultState = {
+  text: "",
+  sortBy: "date",
+  startDate: undefined,
+  endDate: undefined
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+// Store creation
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  }) 
+);
+
+console.log(store.getState());
+
 const demoState = {
   expenses: [{
     id: "asplfplkj",
