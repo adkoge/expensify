7e53748beda8c33f509bd53267ed990c39b9b39a
Alan Koger <adkoge@gmail.com>
2018-12-28 12:39:16 -0500

    Set up provider and created higher order component using connect()

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
-  <div>This is from my dashboard component</div>
+  <div>
+    <ExpenseList />
+  </div>
 );
 
 export default ExpenseDashboardPage;
