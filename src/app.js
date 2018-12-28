76fd5e9651dce398d3dcdfead81a7ca161526fc3
Alan Koger <adkoge@gmail.com>
2018-12-28 13:51:43 -0500

    Added select filters for expenses

--- src/app.js
+++ src/app.js
@@ -11,13 +11,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 250}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 400}));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() =>  {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
