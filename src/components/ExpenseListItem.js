a7207feeb795f1a5a665d5f0a78955e8261b25d5
Alan Koger <adkoge@gmail.com>
2018-12-28 13:29:54 -0500

    Added remove expense button to expense list item and dispatched action

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
+import { removeExpense } from "../actions/expenses";
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
     <b>{description}</b><br/>
     Amount: {amount}<br/>
-    Created At: {createdAt}
+    Created At: {createdAt}<br/>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }));
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
