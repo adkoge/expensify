12da0369e7c84e57b6d1704efc9282014a67d8ca
Alan Koger <adkoge@gmail.com>
2018-12-29 10:19:07 -0500

    Added extra test with default value

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,12 +1,17 @@
 const add = (a, b) => a + b;
-const generateGreeting = (name) => `Hello ${name}!`;
+const generateGreeting = (name = "Anonymous") => `Hello ${name}!`;
 
 test("should add two number", () => {
   const result = add(3, 4);
   expect(result).toBe(7);
 });
 
-test("should return a name", () => {
+test("should generate greeting from name", () => {
   const result = generateGreeting("Alan");
   expect(result).toBe("Hello Alan!");
+});
+
+test("Should generate greeting for no name", () => {
+  const result = generateGreeting();
+  expect(result).toBe("Hello Anonymous!");
 });
