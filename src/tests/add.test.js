c79531c57b80d72e3780eae09c726e658c0c1ff0
Alan Koger <adkoge@gmail.com>
2018-12-29 10:17:51 -0500

    Added jest testing suite and sample test to application.

---
+++ src/tests/add.test.js
@@ -0,0 +1,12 @@
+const add = (a, b) => a + b;
+const generateGreeting = (name) => `Hello ${name}!`;
+
+test("should add two number", () => {
+  const result = add(3, 4);
+  expect(result).toBe(7);
+});
+
+test("should return a name", () => {
+  const result = generateGreeting("Alan");
+  expect(result).toBe("Hello Alan!");
+});
